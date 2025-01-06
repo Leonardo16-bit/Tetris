d11218cc5a2e12c4d2d6bc490a646919cd8712c4
borgo\borgo-sauro <mattiaborgo08gmail.com>
2025-01-06 18:39:17 +0100

    creazione define iniziali

---
+++ Tetris/main.cpp
@@ -0,0 +1,9 @@
+#define IMM2D_WIDTH  820        // Larghezza della finestra del gioco
+#define IMM2D_HEIGHT 600         // Altezza della finestra del gioco
+#define IMM2D_SCALE 1            // Scala per la visualizzazione degli oggetti grafici (impostato su 1, quindi nessuna scala)
+#define IMM2D_IMPLEMENTATION     // Implementazione della libreria di Immediate2D per il rendering grafico
+
+#include <cstdlib>
+#include "immediate2d.h"        // Include la libreria per il rendering grafico 2D
+#include <string>
+#include <Windows.h>             // Include la libreria di Windows per l'accesso alle API di sistema
